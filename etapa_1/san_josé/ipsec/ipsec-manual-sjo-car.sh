443b66b763085b3828e48a626e8439525dda5a59
marcocg-10 <marcalgue@gmail.com>
2026-09-14 22:53:36 -0600

    etapa_1/san_josé: Añadir regla de forwarding para que equipos en la red interna reciban pings.

--- etapa_1/san_josé/ipsec/ipsec-manual-sjo-car.sh
+++ etapa_1/san_josé/ipsec/ipsec-manual-sjo-car.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 ip xfrm state add src 172.24.133.76 dst 172.24.133.84 proto esp spi 0x1001 mode tunnel \
-  enc aes 0x \
-  auth sha256 0x
+  enc aes 0x<CLAVE_AES> \
+  auth sha256 0x<CLAVE_HMAC>
 
 ip xfrm state add src 172.24.133.84 dst 172.24.133.76 proto esp spi 0x1002 mode tunnel \
-  enc aes 0x \
-  auth sha256 0x
+  enc aes 0x<CLAVE_AES> \
+  auth sha256 0x<CLAVE_HMAC>
+
 
 ip xfrm policy add src 10.4.0.0/20 dst 10.4.32.0/20 dir out \
   tmpl src 172.24.133.76 dst 172.24.133.84 proto esp mode tunnel
 
 ip xfrm policy add src 10.4.32.0/20 dst 10.4.0.0/20 dir in \
+  tmpl src 172.24.133.84 dst 172.24.133.76 proto esp mode tunnel
+
+ip xfrm policy add src 10.4.32.0/20 dst 10.4.0.0/20 dir fwd \
   tmpl src 172.24.133.84 dst 172.24.133.76 proto esp mode tunnel
